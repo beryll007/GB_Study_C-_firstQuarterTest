ec8a4fbb19b45d2e71143bf18698dffd4e989b68
Danil Novikov <beryl@live.ru>
2022-07-29 19:41:34 +0300

    File Program.cs added, method finding final array legth added

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿string [] stringArray = {"Russia", "Denmark", "Kazan"};
+int stringLength = 3;
+
+int FindFinalArrayLength (string [] StringArray, int stringLength)
+{   
+    int count = 0;
+    for (int i = 0; i < StringArray.Length; i++)
+    {
+        if (StringArray[i].Length <= stringLength)
+        {
+            count = count + 1;
+        }
+    }
+    return count;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
